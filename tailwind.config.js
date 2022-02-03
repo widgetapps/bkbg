80db626bbd5c81366aa4e02f26e1b3d8c1290aa1
Darryl Desktop <widgetapps@gmail.com>
2022-02-02 21:14:03 -0500

    Just getting started.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'logo': "url('/be-kind-be-good.svg')"
+      }
+    }
+  },
+  plugins: [],
+}
